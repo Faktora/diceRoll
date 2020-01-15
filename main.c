f88e47654d9d64b3e23a41acfcd4402892571dc9
Stefan Karabaliev <stefan.karabata@gmail.com>
2020-01-15 21:58:02 +0000

    Add printf workaround for CLion

--- main.c
+++ main.c
@@ -2,31 +2,35 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define LENGHT 6
+#define LENGTH 6
 
-int rolls[LENGHT];
+int rolls[LENGTH];
 int throws = 0;
 
 //func to make the dice roll
-void roll();
+void roll(void);
 
 //printing func so it does not just spit the numbers from the array
 void translate(const int array[]);
 
 //this should be the func to save dice, rest of the dice should be rerolled (max 3 times)
 //if the player saves all, then his turn must end (later on)
-void keepDice(int ch, int array[]);
+void keepDice(int ch, const int array[]);
 
 int main() {
     int choice = 0;// takes users choice on which dice to keep, rest is getting rerolled!!!
-    roll(rolls);
-    scanf("Which dice do you want to keep?\nChoose between 0 and 5!\n%d", choice);
+    roll();
+    setbuf(stdout, 0);
+    printf("Which dice do you want to keep?\nChoose between 0 and 5!\n");
+    scanf("%d", &choice);
     keepDice(choice, rolls);
     translate(rolls);//calls the printing function to see the rolls
     //testing print
-    for (int j = 0; j < LENGHT; j++) {
+    for (int j = 0; j < LENGTH; j++) {
+        setbuf(stdout, 0);
         printf("%d ", rolls[j]);
     }
+    setbuf(stdout, 0);
     printf("\n");
 
     return 0;
@@ -40,48 +44,54 @@ void roll() {
     for (int i = 0; i <= 5; i++) {
         currRoll = rand() % 6;// save current roll here
         rolls[i] = currRoll; // later on should be:  rolls[i] = rand() % 6;
+        setbuf(stdout, 0);
         printf("%d ", currRoll);//just checking
     }
     printf("\n");//beauty print
 }
 
 void translate(const int array[]) {
-    int tmp = 0;// using it for the switch statement, no better idea atm
     //going through the array taking each element and then putting it in the switch machine for proper print
-    for (int i = 0; i < LENGHT; i++) {
-        tmp = array[i];
+    for (int i = 0; i < LENGTH; i++) {
         //later on i can dump the tmp and just stay with:   switch(array[i])
-        switch (tmp) {
+        switch (array[i]) {
             case 0:
-                printf("%7d", 9);
+                setbuf(stdout, 0);
+                printf("%-7d", 9);
                 break;
             case 1:
-                printf("%7d", 10);
+                setbuf(stdout, 0);
+                printf("%-7d", 10);
                 break;
             case 2:
-                printf("%7s", "Jack");
+                setbuf(stdout, 0);
+                printf("%-7s", "Jack");
                 break;
             case 3:
-                printf("%7s", "Queen");
+                setbuf(stdout, 0);
+                printf("%-7s", "Queen");
                 break;
             case 4:
-                printf("%7s", "King");
+                setbuf(stdout, 0);
+                printf("%-7s", "King");
                 break;
             case 5:
-                printf("%7s", "Ace");
+                setbuf(stdout, 0);
+                printf("%-7s", "Ace");
                 break;
             default:
                 break;
         }
     }
+    setbuf(stdout, 0);
     printf("\n");//beauty print
 }
 
 /*  0 = 9; 1 = 10; 2 = Jack; 3 = Queen; 4 = King; 5 = Ace */
 
-void keepDice(int ch, int array[]) {
-    int tmp[LENGHT];
-    for (int i = 0; i < LENGHT; i++) {
+void keepDice(int ch, const int array[]) {
+    int tmp[LENGTH];
+    for (int i = 0; i < LENGTH; i++) {
         tmp[i] = array[i];
     }
 }
