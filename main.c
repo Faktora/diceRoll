51b059d209f412c2cf2cfefa3ec2729f7e365554
Nedelin Todorov <nedelin.todorov@abv.bg>
2020-01-16 21:52:59 +0100

    reform in point syst

--- main.c
+++ main.c
@@ -29,11 +29,23 @@ bool keepDice(int keep);
  *  Aces are 6 points*/
 void countPoints(const int array[]);
 
+/* these funcs thelp the main func that 
+ * checks if the array is consecutive*/
+int getMin(const int arr[], int number);//finds the min element in the array
+int getMax(const int arr[], int number);//finds the max element in the array
+/* returns true if all array elements are consecutive
+ * returns false if the array elements are not consecutive*/
+bool consecutiveChecker(int arr[], int number);
+/*checks if all array elements are Aces or not*/
+bool grandeChecker(const int array[]);
+
 int main() {
     int numToKeep = 0;// takes users choice on which dice to keep, rest is getting rerolled!!!
     //printf("First roll: \n");
 
     roll();
+    consecutiveChecker(playerOneRolls, LENGTH);
+    grandeChecker(playerOneRolls);
     setbuf(stdout, 0);
     printf("\nHow many dice to save?\n");
     scanf("%d", &numToKeep);
@@ -81,19 +93,19 @@ void translate(const int array[]) {
         if (array[i] == 0) {
             setbuf(stdout, 0);
             printf("%-7d", 9);
-        }else if(array[i] == 1) {
+        } else if (array[i] == 1) {
             setbuf(stdout, 0);
             printf("%-7d", 10);
-        }else if (array[i] == 2) {
+        } else if (array[i] == 2) {
             setbuf(stdout, 0);
             printf("%-7s", "Jack");
-        }else if (array[i] == 3) {
+        } else if (array[i] == 3) {
             setbuf(stdout, 0);
             printf("%-7s", "Queen");
-        }else if (array[i] == 4) {
+        } else if (array[i] == 4) {
             setbuf(stdout, 0);
             printf("%-7s", "King");
-        }else if (array[i] == 5){
+        } else if (array[i] == 5) {
             setbuf(stdout, 0);
             printf("%-7s", "Ace");
         }
@@ -144,23 +156,85 @@ bool keepDice(int keep) {
 void countPoints(const int array[]) {
     int tempSum = 0;
     for (int i = 0; i < LENGTH; i++) {
-        if (array[i] == 0) {
-            if (array[i] == 0) {
-                tempSum += 1;
-            } else if (array[i] == 1) {
-                tempSum += 2;
-            } else if (array[i] == 2) {
-                tempSum += 3;
-            } else if (array[i] == 3) {
-                tempSum += 4;
-            } else if (array[i] == 4) {
-                tempSum += 5;
-            } else if (array[i] == 5) {
-                tempSum += 6;
-            }
-            playerOnePoints = tempSum;
+        if (consecutiveChecker(playerOneRolls, LENGTH)) {
+            tempSum += 20;
+            break;
+        } else if (grandeChecker(array) == true) {
+            tempSum += 50;
+            break;
+        } else if (array[i] == 0) {
+            tempSum += 1;
+        } else if (array[i] == 1) {
+            tempSum += 2;
+        } else if (array[i] == 2) {
+            tempSum += 3;
+        } else if (array[i] == 3) {
+            tempSum += 4;
+        } else if (array[i] == 4) {
+            tempSum += 5;
+        } else if (array[i] == 5) {
+            tempSum += 6;
         }
-        printf("Player one got: %d points this round.\n", playerOnePoints);
-
+        playerOnePoints = tempSum;
     }
+    printf("Player one got: %d points this round.\n", playerOnePoints);
+}
+
+/* the two functions finding min and max number in the array */
+int getMin(const int arr[], int number) {
+    int min = arr[0];
+    for (int i = 1; i < number; i++)
+        if (arr[i] < min)
+            min = arr[i];
+    return min;
+}
+
+int getMax(const int arr[], int number) {
+    int max = arr[0];
+    for (int i = 1; i < number; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+bool consecutiveChecker(int arr[], int number) {
+    if (number < 1)
+        return false;
+
+    /* first we get the minimum number from array */
+    int min = getMin(arr, number);
+
+    /* then we get the biggest number from array */
+    int max = getMax(arr, number);
+
+    /* if max - min + 1 is equal to number,  only then check all elements */
+    if (max - min + 1 == number) {
+        /* Create a temp arr to hold visited flag of all elements.
+           Note that, calloc is used here so that all values are initialized
+           as false */
+        bool *visited = (bool *) calloc(number, sizeof(bool));
+        int i;
+        for (i = 0; i < number; i++) {
+            /* If we see an element again, then return false */
+            if (visited[arr[i] - min] != false)
+                return false;
+            /* If visited first time, then mark the element as visited */
+            visited[arr[i] - min] = true;
+        }
+        /* If all elements occur once, then return true */
+        return true;
+    }
+    return false; // if (max - min  + 1 != n)
+}
+
+bool grandeChecker(const int array[]) {
+    int checker = array[0];
+    for (int i = 0; i < LENGTH; i++) {
+        if (checker == array[i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
