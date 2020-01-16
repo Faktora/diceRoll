7d25affa336dff8c677b587c6fb588c957f4cac3
Nedelin Todorov <nedelin.todorov@abv.bg>
2020-01-16 01:42:09 +0100

    implemented point system

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 
 #define LENGTH 5
 
-int rolls[LENGTH];
-int throws = 0;
+int playerOneRolls[LENGTH];
+int playerOnePoints = 0;
 
 //func to make the dice roll
 void roll(void);
@@ -16,7 +16,18 @@ void translate(const int array[]);
 
 //this should be the func to save dice, rest of the dice should be rerolled (max 3 times)
 //if the player saves all, then his turn must end (later on)
+//boolean so it can keep track of numbers of rolls
 bool keepDice(int keep);
+
+//implement a pointing system :
+/*POINT SUMMARY
+ *  9s are 1 point
+ *  10s are 2 points
+ *  Jacks are 3 points
+ *  Queens are 4 points
+ *  Kings are 5 points
+ *  Aces are 6 points*/
+void countPoints(int array[]);
 
 int main() {
     int numToKeep = 0;// takes users choice on which dice to keep, rest is getting rerolled!!!
@@ -34,11 +45,11 @@ int main() {
         keepDice(numToKeep);
     }
 
-    translate(rolls);//calls the printing function to see the rolls
+    translate(playerOneRolls);//calls the printing function to see the rolls
     //testing print
     for (int j = 0; j < LENGTH; j++) {
         setbuf(stdout, 0);
-        printf("%d ", rolls[j]);
+        printf("%d ", playerOneRolls[j]);
     }
     setbuf(stdout, 0);
     printf("\n");
@@ -47,18 +58,18 @@ int main() {
 }
 
 //suitable only for first time roll to fill the array with random numbers
-//not going to work with rerolls probably
-//might work if I put the old rolls at the start and reroll just the last spaces in the array
+//not going to work with re-rolls probably
+//might work if I put the old rolls at the start and re-roll just the last spaces in the array
 void roll() {
     int currRoll;//int to save the current roll used for testing now
     srand((unsigned int) time(NULL));
     for (int i = 0; i < LENGTH; i++) {
         currRoll = rand() % 6;// save current roll here
-        rolls[i] = currRoll; // later on should be:  rolls[i] = rand() % 6
+        playerOneRolls[i] = currRoll; // later on should be:  rolls[i] = rand() % 6
     }
     setbuf(stdout, 0);
     printf("\n");//beauty print
-    translate(rolls);
+    translate(playerOneRolls);
 }
 
 void translate(const int array[]) {
@@ -122,10 +133,46 @@ bool keepDice(int keep) {
             }
             if (!isKeep) {
                 srand((unsigned int) time(NULL));
-                rolls[j] = rand() % 6;
+                playerOneRolls[j] = rand() % 6;
             }
         }
-        translate(rolls);
+        translate(playerOneRolls);
         return false;
     }
+}
+
+/*POINT SUMMARY
+ *  9s are 1 point
+ *  10s are 2 points
+ *  Jacks are 3 points
+ *  Queens are 4 points
+ *  Kings are 5 points
+ *  Aces are 6 points*/
+void countPoints(int array[]){
+    int tempSum = 0;
+    for (int i = 0; i < LENGTH; i++) {
+        switch (array[i]){
+            case 0:
+                tempSum += 1; // one point for 9s
+                break;
+            case 1:
+                tempSum += 2; // two points for 10s
+                break;
+            case 2:
+                tempSum += 3; // three points for Jacks
+                break;
+            case 3:
+                tempSum += 4; // four points for Queens
+                break;
+            case 4:
+                tempSum += 5; // five points for Kings
+                break;
+            case 5:
+                tempSum += 6; // six points for Aces
+                break;
+        }
+        playerOnePoints = tempSum;
+    }
+    printf("Player one got: %d points this round.\n", playerOnePoints);
+
 }
