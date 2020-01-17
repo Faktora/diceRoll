dfed4c4390c5e3ffa1529cda0a90782effb9ac35
Nedelin Todorov <nedelin.todorov@abv.bg>
2020-01-17 14:34:50 +0100

    code reFactor

--- main.c
+++ main.c
@@ -38,23 +38,25 @@ int getMax(const int arr[], int number);//finds the max element in the array
  * returns false if the array elements are not consecutive*/
 bool consecutiveChecker(int arrCons[], int number);
 /*checks if all array elements are Aces or not*/
-bool grandeChecker(const int arrayGrande[],  int arrayLength);
+bool grandeChecker(const int isGrande[], int arrayLength);
 
 bool checkGameFinished(int gameChoice);
 
 int main() {
 
-    srand((unsigned int) time(NULL));// starting here for test
+    srand((unsigned int) time(NULL));// starting here for more randomness
     int numToKeep = 0;// takes users choice on which dice to keep, rest is getting rerolled!!!
-    //printf("First roll: \n");
     int rollsMade = 0;
     int userGameChoice = 0;
     bool gameFinished = false;
-    printf("What you wanna play man?\n");
-    printf("");
+
+    printf(" *** GAME MODES ***\n1) Normal point summary (100points)\n2) Table point summary\n");
+    printf("\n");
     scanf("%d", &userGameChoice);
-    while(!gameFinished) {
-        grandeChecker(playerRolls[currentPlayer], LENGTH);
+
+    while (!gameFinished) {
+        printf("Player %d rolls now! Good luck!\n", currentPlayer + 1);
+        //grandeChecker(playerRolls[currentPlayer], LENGTH);
         setbuf(stdout, 0);
         bool isTurnEnded = false;
         while (!isTurnEnded) {
@@ -99,37 +101,40 @@ void translate(const int array[]) {
     setbuf(stdout, 0);
     printf("\n");//beauty print
 }
-/*  0 = 9; 1 = 10; 2 = Jack; 3 = Queen; 4 = King; 5 = Ace */
+
 bool keepDice(int keep, int *rollsAmount) {
     int currDice;
     *rollsAmount += 1;
-    if (keep == 5 || *rollsAmount == 4) {
+    if (*rollsAmount == 4) {
         *rollsAmount = 0;
         return true;
     } else {
         // search
-        if(*rollsAmount != 1){
+        if (*rollsAmount != 1) {
             setbuf(stdout, 0);
             printf("\nHow many dice to save?\n");
             scanf("%d", &keep);
-        }
-
-        int arrayKeep[keep];
-        for (int k = 0; k < keep; k++) {
-            setbuf(stdout, 0);
-            printf("\nWhich one to keep?\n");
-            scanf("%d", &currDice);
-            arrayKeep[k] = currDice;
-        }
-        for (int j = 0; j < 5; j++) {//
-            bool isKeep = false;
-            for (int i = 0; i < keep; i++) {
-                if (arrayKeep[i] == j) {
-                    isKeep = true;
-                }
+        }if(keep == 5){ //if no if here > 5 inputs on which to save
+            *rollsAmount = 0;
+            return true;
+        }else{
+            int arrayKeep[keep];
+            for (int k = 0; k < keep; k++) {
+                setbuf(stdout, 0);
+                printf("\nWhich one to keep?\n");
+                scanf("%d", &currDice);
+                arrayKeep[k] = currDice;
             }
-            if (!isKeep) {
-                playerRolls[currentPlayer][j] = rand() % 6;
+            for (int j = 0; j < 5; j++) {//
+                bool isKeep = false;
+                for (int i = 0; i < keep; i++) {
+                    if (arrayKeep[i] == j) {
+                        isKeep = true;
+                    }
+                }
+                if (!isKeep) {
+                    playerRolls[currentPlayer][j] = rand() % 6;
+                }
             }
         }
         printf("Roll: %d\n", *rollsAmount);
@@ -146,28 +151,34 @@ bool keepDice(int keep, int *rollsAmount) {
  *  Kings are 5 points
  *  Aces are 6 points*/
 void countPoints() {
-    int tempSum = 0;
+    int roundPoints = 0;
     for (int i = 0; i < LENGTH; i++) {
-        if (consecutiveChecker(playerRolls[currentPlayer], LENGTH)) {
+        int tempSum = 0; // it resets on each and every re-roll
+        //need to implement if else statement to check if some of the special hands are made on 1st throw
+        /*if (rollsMade == 1) more points else fewer points*/
+        if (consecutiveChecker(playerRolls[currentPlayer],LENGTH)) { // Straight from 9 to King or from 10 to Ace forward and backwards
             tempSum += 20;
-        } else if (grandeChecker(playerRolls[currentPlayer], LENGTH)) {
-            tempSum += 50;
-        } else if (playerRolls[currentPlayer][i] == 0) {
+        } else if (grandeChecker(playerRolls[currentPlayer], LENGTH)) { // Works only with 5x Ace
+            tempSum += 50; // not working properly: 5x Ace != 250 !!!
+        } else if (playerRolls[currentPlayer][i] == 0) { // 9
             tempSum += 1;
-        } else if (playerRolls[currentPlayer][i] == 1) {
+        } else if (playerRolls[currentPlayer][i] == 1) { // 10
             tempSum += 2;
-        } else if (playerRolls[currentPlayer][i] == 2) {
+        } else if (playerRolls[currentPlayer][i] == 2) { // Jack
             tempSum += 3;
-        } else if (playerRolls[currentPlayer][i] == 3) {
+        } else if (playerRolls[currentPlayer][i] == 3) { // Queen
             tempSum += 4;
-        } else if (playerRolls[currentPlayer][i] == 4) {
+        } else if (playerRolls[currentPlayer][i] == 4) { // King
             tempSum += 5;
-        } else if (playerRolls[currentPlayer][i] == 5) {
+        } else if (playerRolls[currentPlayer][i] == 5) { // Ace
             tempSum += 6;
         }
+        roundPoints += tempSum;
         playerPoints[currentPlayer] += tempSum;
     }
-    printf("Player %d got: %d points this round.\n", currentPlayer+1, playerPoints[currentPlayer]);
+    printf("Player %d made: %d points this round and total: %d points!\n", currentPlayer + 1,
+           roundPoints, playerPoints[currentPlayer]);
+    //printf("Player %d got: %d points so far.\n", currentPlayer + 1, playerPoints[currentPlayer]);
 }
 
 /* the two functions finding min and max number in the array */
@@ -191,38 +202,37 @@ bool consecutiveChecker(int arr[], int number) {
     if (number < 1)
         return false;
 
-    /* first we get the minimum number from array */
+    /* min number from the array */
     int min = getMin(arr, number);
 
-    /* then we get the biggest number from array */
+    /* max number from the array */
     int max = getMax(arr, number);
 
     /* if max - min + 1 = number,  only then check all elements */
     if (max - min + 1 == number) {
-        /* Create a temp arr to hold visited flag of all elements.
-           Note that, calloc is used here so that all values are initialized
-           as false */
+        /* the temp arr saves the visit flag of elements
+           calloc used to initialize the elements as false*/
         bool *visited = (bool *) calloc(number, sizeof(bool));
         for (int i = 0; i < number; i++) {
             /* If we see an element again, then return false */
-            if (visited[arr[i] - min] != false){
+            if (visited[arr[i] - min] != false) {
                 return false;
-            }else {
+            } else {
                 visited[arr[i] - min] = true;
             }
-            /* If visited first time, then mark the element as visited */
+            /* after first visit flag as such */
         }
-        /* If all elements occur once, then return true */
+        /* if all elements are unique return true */
         return true;
     }
     return false; // if (max - min  + 1 != number)
 }
 
-bool grandeChecker(const int arrayGrande[], int arrayLength) {
-    int checker = 5;
+bool grandeChecker(const int isGrande[], int arrayLength) {
+    const int checker = 5;
     for (int i = 0; i < arrayLength; i++) {
-        if (checker == arrayGrande[i]) {
-            continue;
+        if (checker == isGrande[i]) {
+
         } else {
             return false;
         }
@@ -230,10 +240,11 @@ bool grandeChecker(const int arrayGrande[], int arrayLength) {
     return true;
 }
 
-bool checkGameFinished(int gameChoice){
-    switch (gameChoice){
-        case 1: countPoints();
-            if (playerPoints[currentPlayer] >= 100){
+bool checkGameFinished(int gameChoice) {
+    switch (gameChoice) {
+        case 1:
+            countPoints();
+            if (playerPoints[currentPlayer] >= 100) {
                 return true;
             }
     }
